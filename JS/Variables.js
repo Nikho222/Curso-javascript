fe2470666b527787ab205b97964bb8218eb7dcda
Brayllan Vidal <brayllannicolas.vidal@alumnos.ulagos.cl>
2024-08-28 19:39:26 -0400

    Subien archivos modificados y archivo de variables

---
+++ JS/Variables.js
@@ -0,0 +1,29 @@
+//Variables que pueden cambir con el tiempo
+var estatura = 1.66
+let peso = 80
+
+
+const nombre = "brayllan"
+//Declarando vriables
+
+var edad
+let ciudad
+
+//Inicilizando variable
+edad = 29
+ciudad = "Quellón"
+
+
+//Declarción e inicialización de una variable
+const apellido = "Vidal"
+
+//Contatenaandoi variables de tipo string
+const nombrecompleto = nombre + " " + apellido
+console.log(nombrecompleto);
+
+let docente = 'Tomas', age = 25, signaturas = ['IA', 'Seguridad Informática']
+
+
+//Entendiendo el concepto de Hoisting(Sólo con var)
+console.log(n);
+var n = 10;
