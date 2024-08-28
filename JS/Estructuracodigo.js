73c55149f7cfcc940abab765ad2b33b4cea8b99e
Brayllan Vidal <brayllannicolas.vidal@alumnos.ulagos.cl>
2024-08-28 19:43:32 -0400

    Subiendo archivo estructura codigo y modificación de holamundo

---
+++ JS/Estructuracodigo.js
@@ -0,0 +1,4 @@
+console.log("Estos son números");
+[1, 2, 3, 4].forEach(console.log);
+console.log("Estos son números");
+[1, 2, 3, 4].forEach(console.log);
--- JS/hola.js
+++ JS/hola.js
@@ -1,7 +1,3 @@
 alert("Hola Mundo! - JS Externo")
 console.log("Hola Mundo")
 
-console.log("Estos son números");
-[1, 2, 3, 4].forEach(console.log);
-console.log("Estos son números");
-[1, 2, 3, 4].forEach(console.log);
