04d328f74a995878d8d3435aa3213ffbc368a788
Brayllan Vidal <brayllannicolas.vidal@alumnos.ulagos.cl>
2024-09-04 19:41:23 -0400

    Segundo ejemplo uso scope

---
+++ JS/scope2.js
@@ -0,0 +1,13 @@
+var comic = 'X-Men' 
+
+function comprar () {
+    console.log('compraste el comic ${comic}')
+    //compraste ek comic x-men
+}
+function leer () {
+    console.log('estas leyendo ${comic}')
+    //estas leyendo el comic
+}
+
+comprar ()
+leer ()
