428e6326f96818f2181b5dcb5f699f2fe96adc00
Brayllan Vidal <brayllannicolas.vidal@alumnos.ulagos.cl>
2024-09-04 19:24:52 -0400

    Ejemplo de uso Scope

---
+++ JS/scope.js
@@ -0,0 +1,10 @@
+function teamCap () {
+    var heroe = "Capitan America";
+    console.log(heroe);
+    //Capitan America
+}
+function teamIronMan () {
+    var heroe = "Iron Man";
+    console.log(heroe);
+    //Iron Man
+}
